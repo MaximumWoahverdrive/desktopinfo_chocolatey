cd37647bbed9030f5aa5c68077dc93ecf4a6be5c
MaximumWoahverdrive <jo.schmeder.jo@gmail.com>
2022-03-04 22:41:04 +0100

    Initial commit as of new maintainership

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+$url        = 'https://www.glenn.delahoy.com/downloads/desktopinfo/DesktopInfo210.zip' # download url, HTTPS preferred
+
+$packageZipArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  url           = $url
+  validExitCodes= @(0, 3010, 1641)
+  Checksum = 'fedd560dc83574ed1e2bf7f415a17e73d0fdb5c2287869116b960bd2b48f30ba'
+  ChecksumType = 'SHA256'
+}
+
+Install-ChocolateyZipPackage @packageZipArgs
+	
